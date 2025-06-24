4cb4e8aa61b7f4f1a00df74981ccffe8b86abdc4
FixingWoo <fixingwoo@gmail.com>
2025-06-24 13:34:26 +0900

    feat: Column

---
+++ src/stories/Column.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Column } from '../components/Column';
+
+import '@/tokens/index.scss';
+import '@/styles/index.scss';
+
+const meta: Meta<typeof Column> = {
+  title: 'Components/Column',
+  component: Column,
+  tags: ['autodocs'],
+};
+export default meta;
+
+type Story = StoryObj<typeof Column>;
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <Column padding="s" background="neutral-weak">
+          자식 1
+        </Column>
+        <Column padding="s" background="neutral-medium">
+          자식 2
+        </Column>
+        <Column padding="s" background="neutral-strong">
+          자식 3
+        </Column>
+      </>
+    ),
+  },
+};
